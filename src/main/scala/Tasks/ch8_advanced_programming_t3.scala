69f393f80f2ca10787ba235f93c4a6bcb4591839
oljh <https://github.com/oljh/MyCheese.git>
2020-01-14 11:38:18 +0300

    Spark sh

--- src/main/scala/Tasks/ch8_advanced_programming_t3.scala
+++ src/main/scala/Tasks/ch8_advanced_programming_t3.scala
@@ -7,6 +7,8 @@ object ch8_advanced_program_t3 {
 
   def main(args: Array[String]) {
     val spark = SparkSession.builder.appName("trDig").master("local").getOrCreate()
+    val shFile = "src/res/scripts/translate_digit.sh" // Should be some file on your system
+    val shData = spark.read.textFile(shFile).cache()
 
 
     val nums: List[Integer] = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
@@ -17,9 +19,9 @@ object ch8_advanced_program_t3 {
     def bashShell(command: String) = Seq("bash", "-c", s"$command").!!.trim
     //spark.sparkContext.parallelize(br.lines()).foreach(line => println(s"$line") )
 
-    println(bashShell("chmod +x src/res/scripts/translate_digit.sh"))
+    println(bashShell("chmod +x src/res/scripts/translate_digit2.sh"))
 
-    println(bashShell("./src/res/scripts/translate_digit.sh"))
+    println(bashShell("./src/res/scripts/translate_digit2.sh"))
 
     println(bashShell("echo `date`"))
   }
